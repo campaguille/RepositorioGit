7ef7b65bb0905f63ebdd2ab67df13af7ff3ed3b1
Guillermo Campaña Herrero <guillercahe@gmail.com>
2025-04-01 18:45:45 +0100

    v2

--- Programación/untitled/src/ClasePrincipal.java
+++ Programación/untitled/src/ClasePrincipal.java
@@ -3,5 +3,6 @@
 public class ClasePrincipal {
     public static void main(String[] args) {
         System.out.println("Hola a todos esto es un HolaMunDO");
+        System.out.println("Hola a todos esto es un segundo HolaMunDO");
     }
 }
